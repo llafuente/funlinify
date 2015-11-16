7dcf1696bc028bcf9043f58860347a9f64068bdc
Luis Lafuente <luis.lafuente@inetsys.es>
2015-11-16 13:58:32 +0100

    unkown changes, i fotgot what i was doing... maybe when have some time...

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var parse_ast = require('esprima').parse,
-    escodegen = require("escodegen"),
+var utils = require("esprima-ast-utils"),
     object = require("object-enhancements"),
     validators = {},
     verbose = false,
@@ -7,76 +6,10 @@ var parse_ast = require('esprima').parse,
     is_function = selector("function"),
     hash_id = 0;
 
-var objectKeys = Object.keys || function (obj) {
-    var keys = [];
-    for (var key in obj) keys.push(key);
-    return keys;
-};
-var forEach = function (xs, fn) {
-    if (xs.forEach) return xs.forEach(fn);
-    for (var i = 0; i < xs.length; i++) {
-        fn.call(xs, xs[i], i, xs);
-    }
-};
-
-// set $parent node
-function parentize(node) {
-    traverse(node, function(node, parent) {
-        node.$parent = parent;
-    });
-}
-
-var node_ids = 0;
-// set a unique $id
-function idze(node) {
-    traverse(node, function(node, parent) {
-        if (node.$id === undefined) {
-            node.$id = ++node_ids;
-        }
-    });
-}
-
-// filter ast
-function filter(node, callback) {
-    var out = [];
-
-    traverse(node, function(node, parent) {
-        if (callback(node)) {
-            out.push(node);
-        }
-    });
-
-    return out.length ? out : null;
-}
-
-// traverse ast
-// callback: function(node, parent, property, index, depth) -> Boolean (false to stop)
-function traverse(node, callback, depth, recursive) {
-
-    depth = depth || 0;
-    forEach(objectKeys(node), function (key) {
-        // do not follow internal vars
-        if (key[0] === '$') return;
-
-        var child = node[key];
-        if (Array.isArray(child)) {
-            forEach(child, function (c, idx) {
-                if (c && typeof c.type === 'string') {
-                    callback(c, node, key, idx, depth);
-                    recursive !== false && traverse(c, callback, depth + 1, recursive);
-                }
-            });
-        }
-        else if (child && typeof child.type === 'string') {
-            callback(child, node, key, null, depth);
-            recursive !== false && traverse(child, callback, depth + 1, recursive);
-        }
-    });
-}
 
 function find(node, needle) {
     var found = false;
-    traverse(node, function(nn) {
+    utils.traverse(node, function(nn) {
         if (nn.$id === needle.$id) {
             found = true;
         }
@@ -84,53 +17,6 @@ function find(node, needle) {
 
     return found;
 }
-
-
-/**
- * Rename variables
- */
-function rename(node, replacements) {
-    traverse(node, function(node) {
-        if (node.type == "Identifier"
-            // fix rename inside objects: {name: xxx}
-            && node.$parent.type !== "Property"
-            // fix rename objects: object.name
-            && node.$parent.type !== "MemberExpression") {
-            var owner = getParent(node, is_function);
-
-            if (owner.$id === root.$id && replacements[node.name]) {
-                node.name = replacements[node.name];
-            }
-        }
-    });
-}
-
-// function must exists in the file atm
-function getFunction(node, name) {
-    // search nearest function
-    var fn = filter(node, function(node) {
-        return is_function(node) && node.id && node.id.name == name;
-    });
-
-    if (fn && fn.length) {
-        return fn[0];
-    }
-
-    return null;
-}
-
-function getParent(node, callback) {
-    var n = node;
-    while(n.$parent) {
-        n = n.$parent;
-        if (callback(n)) {
-            return n;
-        }
-    }
-
-    return null;
-}
-
 
 ///
 ///
@@ -140,35 +26,6 @@ function getParent(node, callback) {
 function get_hash() {
     return "__" + (++hash_id) + "__";
 }
-
-
-function clone_node(node) {
-    var copy = {};
-    forEach(objectKeys(node), function(name) {
-        // ignore auto generated
-        if (name[0] === "$") return;
-
-        var value = node[name],
-            cvalue;
-
-        //recursion!
-        if (Array.isArray(value)) {
-            cvalue = value.map(clone_node);
-        } else if ("object" === object.typeof(value)) {
-            cvalue = clone_node(value);
-        }
-
-        // Note that undefined fields will be visited too, according to
-        // the rules associated with node.type, and default field values
-        // will be substituted if appropriate.
-        copy[name] = cvalue || value;
-    });
-
-    copy.$cloned = true;
-
-    return copy;
-}
-
 
 function replace(node, property, index, new_node) {
     if (index === null) {
@@ -201,43 +58,22 @@ function insertAfter(node, property, index, new_node) {
 
 function returnToExpression(root, output_var, break_label) {
 
-    traverse(root, function(node, parent, property, index, depth) {
+    utils.traverse(root, function(node, parent, property, index, depth) {
         if (node.type == "ReturnStatement") {
-            var owner = getParent(node, is_function);
+            var owner = utils.getParent(node, is_function);
 
-            if (owner.$id === root.$id) {
+            if (owner.$id === root.body[0].$id) {
 
-                //console.log("****************************");
-                //console.log(node, parent, property, index, depth);
-                //console.log("****************************");
-                //console.log(require("util").inspect(parent, {depth: null}));
-                //console.log(require("util").inspect(node, {depth: null}));
+                var program = utils.parse(break_label+ ": {\n" +
+                    output_var + " = " + utils.getCode(node.argument) +
+                    ";break " + break_label+ ";}\n"
+                );
 
-                replace(parent, property, index,  {
-                    type: 'ExpressionStatement',
-                    expression: {
-                        type: 'AssignmentExpression',
-                        operator: '=',
-                        left: {
-                            type: 'Identifier',
-                            name: output_var
-                        },
-                        right: node.argument
-                    },
-                });
+                program = utils.toProgram(program.body[0].body);
 
-                insertAfter(parent, property, index, {
-                    type: 'BreakStatement',
-                    label: {
-                        type: 'Identifier',
-                        name: break_label
-                    }
-                });
 
+                utils.replace(node, program);
             }
-
-            //console.log(require("util").inspect(parent, {depth: null}));
-            //console.log(escodegen.generate(parent));
         }
     });
 }
@@ -274,18 +110,15 @@ function getArguments(node) {
 function transform (content, transformOptions, done) {
         //parse ast
 
-        var root = parse_ast(content, {comment: true, loc: true}),
+        var root = utils.parse(content),
             file = require("path").basename(transformOptions.file);
 
-        idze(root);
-        parentize(root);
-
-        traverse(root, function(node, parent, property, index, depth) {
+        utils.traverse(root, function(node, parent, property, index, depth) {
             verbose && console.log("#" + node.$id, node.type, property, index);
 
             if (node.type == "CallExpression") {
-                var fn = getFunction(root, node.callee.name),
-                    owner = getParent(node, is_function);
+                var fn = utils.getFunction(root, node.callee.name),
+                    owner = utils.getParent(node, is_function);
 
 
 
@@ -294,43 +127,42 @@ function transform (content, transformOptions, done) {
                     ||
                     (fn && owner && owner.id && owner.id.name != node.callee.name) // no recursion
                 ) {
-                    var hash = get_hash(),
-                        return_var = hash + "return",
-                        cfun = clone_node(fn),
-                        fun_args = getArguments(cfun),
-                        call_args,
-                        fun_hargs = fun_args.map(function(x) { return hash + x; }),
-                        replacements = object.combine(fun_args, fun_hargs);
-
-                    idze(cfun);
-                    parentize(cfun);
-
                     try {
                         call_args = getArguments(node);
                     } catch(e) {
-                        console.log(file + "@" + node.loc.start.line + "  " + node.callee.name + " cannot be inline: " + e.message);
-
+                        console.info("(info) " + file + "@" + node.loc.start.line + "  " + node.callee.name + " cannot be inline: " + e.message);
                         return;
                     }
 
-                    console.log(file + "@" + node.loc.start.line + " Calling: ", node.callee.name);
-                    //console.log(" --> Arguments: ", replacements);
-
-                    rename(cfun, replacements);
-
-                    var args_to_var = fun_hargs.map(function(v, k) {
-                        return v + " = " + call_args[k];
-                    });
-
-                    args_to_var.push(return_var);
-
-                    args_to_var = "var " + args_to_var.join(",");
-
-                    returnToExpression(cfun, hash + "return", hash);
-
-                    var statement = getParent(node, function(node) { return node.type === "BlockStatement"; });
+                    var statement = utils.getParent(node, function(node) { return node.type === "BlockStatement"; });
 
                     if (statement) {
+
+                        var hash = get_hash(),
+                            return_var = hash + "return",
+                            cfun = utils.toProgram(fn),
+                            fun_args = getArguments(cfun),
+                            call_args,
+                            fun_hargs = fun_args.map(function(x) { return hash + x; }),
+                            replacements = object.combine(fun_args, fun_hargs);
+
+                        console.info(file + "@" + node.range + " Calling: ", node.callee.name);
+
+                        if (Object.keys(replacements).length) {
+                            console.log("replacements", replacements);
+                            utils.renameVariable(cfun, replacements);
+                        }
+
+                        var args_to_var = fun_hargs.map(function(v, k) {
+                            return v + " = " + call_args[k];
+                        });
+
+                        args_to_var.push(return_var);
+
+                        args_to_var = "var " + args_to_var.join(",");
+
+                        returnToExpression(cfun, hash + "return", hash);
+
                         //console.log("statement", statement);
 
                         var fst_property,
@@ -338,42 +170,46 @@ function transform (content, transformOptions, done) {
 
                         // find where am i in this block statement
                         //loop first level only, and try to find me!
-                        traverse(statement, function(st_node, st_parent, st_property, st_index, st_depth) {
+                        utils.traverse(statement, function(st_node, st_parent, st_property, st_index, st_depth) {
                             if (find(st_node, node)) {
-                                fst_property = st_property,
+                                fst_property = st_property;
                                 fst_index = st_index;
                             }
                         }, 0, false);
 
-                        if (fst_property) {
-                            replace(parent, property, index, {
+                        utils.detach(node, fst_property);
+
+                        //utils.replace(node, return_var);
+                        /*
+                        replace(parent, property, index, {
+                            type: 'Identifier',
+                            name: return_var,
+                        });
+                        */
+
+
+                        console.log(require("util").inspect(root, {depth: null, colors: true}));
+                        process.exit();
+
+
+                        cfun.leadingComments = [
+                            {
+                                "type": "Line",
+                                "value": "inline CallExpression() - " + node.callee.name,
+                            }
+                        ];
+
+                        insertBefore(statement, fst_property, fst_index, {
+                            type: 'LabeledStatement',
+                            label: {
                                 type: 'Identifier',
-                                name: return_var,
-                            });
+                                name: hash
+                            },
+                            body: cfun.body
+                        });
+                        insertBefore(statement, fst_property, fst_index, parse_ast(args_to_var));
 
-
-                            cfun.leadingComments = [
-                                {
-                                    "type": "Line",
-                                    "value": "inline CallExpression() - " + node.callee.name,
-                                }
-                            ];
-
-                            insertBefore(statement, fst_property, fst_index, {
-                                type: 'LabeledStatement',
-                                label: {
-                                    type: 'Identifier',
-                                    name: hash
-                                },
-                                body: cfun.body
-                            });
-                            insertBefore(statement, fst_property, fst_index, parse_ast(args_to_var));
-
-                            idze(statement);
-                            parentize(statement);
-
-                            //console.log(escodegen.generate(statement));
-                        }
+                        //console.log(escodegen.generate(statement));
                     }
                 } else if (node.callee.type == "MemberExpression") {
                     console.log(file + "@" + node.loc.start.line + " ignore-call: cant resolve a MemberExpression");
@@ -387,8 +223,8 @@ function transform (content, transformOptions, done) {
 
         //console.log(escodegen.generate(root));
 
-        done(null, escodegen.generate(root, {compact: false, comment: true}));
-    };
+        done(null, root.$code);
+    }
 
 
 module.exports = require("browserify-transform-tools").makeStringTransform("funlinify", {}, transform);
